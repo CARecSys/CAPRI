c45a1ffd59a9debc8b3d564f235605aaa7595542
Ali Tourani <a.tourani1991@gmail.com>
2021-08-27 02:08:16 +0430

    fix: delete unused file

--- _archive.py
+++
@@ -1,9 +0,0 @@
-# Deleted codes
-
-# Fetching the list of files in Data directory
-# dataFiles = os.listdir('./Data')
-
-# Removing their .txt extentions to fetch their names
-# dataVariations = [dataFile.replace('.txt', '') for dataFile in dataFiles]
-
-# coverage = ', '.join(map(str, datasets[dataset])) ===> get the coverage area of parameters
