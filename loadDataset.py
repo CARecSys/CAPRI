121cfe8615fdc354be1b07ec6b754560526a75f5
Ali Tourani <a.tourani1991@gmail.com>
2021-08-27 01:58:25 +0430

    feat: improved loadDataset

--- loadDataset.py
+++ loadDataset.py
@@ -3,6 +3,7 @@ from config import datasets, dataDirectory
 
 
 def loadDataset(datasetName):
+    datasetFiles = {}
     # If model was not among supported models
     supportedDatasets = list(datasets.keys())
     if not datasetName in supportedDatasets:
@@ -10,8 +11,11 @@ def loadDataset(datasetName):
         return
     # Otherwise, list the files existing in dataset directories
     print(f'Preparing {datasetName} dataset items ...')
-    datasetFiles = os.listdir(f'{dataDirectory}/{datasetName}')
-    print(datasetFiles)
-
-
-loadDataset('Yelp')
+    fileList = os.listdir(f'{dataDirectory}/{datasetName}')
+    # Create a dictionary of dataset items
+    for file in fileList:
+        fileName = file.split('.')[0]
+        filePath = dataDirectory + file
+        # Add the absolute path to the items: e.g. 'checkins.txt' ==> 'C\\...\\Datacheckins.txt'
+        datasetFiles[fileName] = filePath
+    return datasetFiles
