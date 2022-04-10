6f8c44bf23d86220c076a89006ed14e4fabf9133
Ali Tourani <a.tourani1991@gmail.com>
2022-04-10 11:09:28 +0200

    fix: improve web-page

--- docs/assets/contact.php
+++ docs/assets/contact.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contactformtestgg@gmail.com";
+        $recipient = "caprirecsys@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
